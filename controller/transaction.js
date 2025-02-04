a7b1e3130825518db4483e1746a809929c0cab42
@undeath-cyber <gabrielxjonathan.12@gmail.com>
2025-02-04 21:35:58 +0700

    fix(date)

--- controller/transaction.js
+++ controller/transaction.js
@@ -6,26 +6,45 @@ const { isAuthenticated } = require('../middleware/auth');
 const catchAsyncErrors = require('../middleware/catchAsyncErrors');
 const ErrorHandler = require('../utils/ErrorHandler');
 const Validator = require('fastest-validator');
+const moment = require('moment');
 const v = new Validator();
 
-// Create transaction
+const validateAndFormatDate = (value, errors) => {
+  if (!value) return value; // Jika tidak ada nilai, biarkan null/undefined
+
+  // Pastikan format MM/DD/YYYY
+  if (!moment(value, 'MM/DD/YYYY', true).isValid()) {
+    return [{ type: 'datePattern', expected: 'MM/DD/YYYY', actual: value }];
+  }
+
+  // Konversi ke format YYYY-MM-DD
+  const formattedDate = moment(value, 'MM/DD/YYYY').format('YYYY-MM-DD');
+  return formattedDate;
+};
+
+// Schema validasi transaksi
+const transactionSchema = {
+  mid: { type: 'string', empty: false },
+  tid: { type: 'string', optional: true },
+  transaction_type: { type: 'string', empty: false },
+  batch: { type: 'string', optional: true },
+  amount: { type: 'number', min: 0 },
+  net_amount: { type: 'number', min: 0 },
+  mdr: { type: 'number', min: 0, optional: true },
+  status: { type: 'string', empty: false },
+  date: {
+    type: 'string',
+    optional: true,
+    custom: validateAndFormatDate,
+  },
+  difference: { type: 'number', optional: true },
+};
+
+// Endpoint untuk membuat transaksi
 router.post(
   '/',
   isAuthenticated,
   catchAsyncErrors(async (req, res, next) => {
-    const transactionSchema = {
-      mid: { type: 'string', empty: false },
-      tid: { type: 'string', optional: true },
-      transaction_type: { type: 'string', empty: false },
-      batch: { type: 'string', optional: true },
-      amount: { type: 'number', min: 0 },
-      net_amount: { type: 'number', min: 0 },
-      mdr: { type: 'number', min: 0, optional: true },
-      status: { type: 'string', empty: false },
-      date: { type: 'string', optional: true },
-      difference: { type: 'number', optional: true },
-    };
-
     const { body } = req;
     const validationResponse = v.validate(body, transactionSchema);
 
@@ -50,85 +69,12 @@ router.post(
         });
       }
 
+      // Simpan transaksi ke database
       const transaction = await Transaction.create(body);
       res.status(201).json({
         code: 201,
         status: 'success',
         data: transaction,
-      });
-    } catch (error) {
-      return next(new ErrorHandler(error.message, 500));
-    }
-  })
-);
-
-// Update transaction
-router.put(
-  '/:id',
-  isAuthenticated,
-  catchAsyncErrors(async (req, res, next) => {
-    const transactionSchema = {
-      mid: { type: 'string', empty: false, optional: true },
-      tid: { type: 'string', optional: true },
-      transaction_type: { type: 'string', empty: false, optional: true },
-      batch: { type: 'string', optional: true },
-      amount: { type: 'number', min: 0, optional: true },
-      net_amount: { type: 'number', min: 0, optional: true },
-      mdr: { type: 'number', min: 0, optional: true },
-      status: { type: 'string', empty: false, optional: true },
-      date: { type: 'string', optional: true },
-      difference: { type: 'number', optional: true },
-    };
-
-    const { body } = req;
-    const transactionId = req.params.id;
-
-    // Validasi input
-    const validationResponse = v.validate(body, transactionSchema);
-    if (validationResponse !== true) {
-      return res.status(400).json({
-        code: 400,
-        status: 'error',
-        data: {
-          error: 'Validation failed',
-          details: validationResponse,
-        },
-      });
-    }
-
-    try {
-      // Cari transaksi berdasarkan ID
-      const transaction = await Transaction.findById(transactionId);
-      if (!transaction) {
-        return res.status(404).json({
-          code: 404,
-          status: 'error',
-          data: { error: 'Transaction not found' },
-        });
-      }
-
-      // Jika transaction_type diubah, pastikan valid
-      if (body.transaction_type) {
-        const transactionType = await TransactionType.findById(body.transaction_type);
-        if (!transactionType) {
-          return res.status(404).json({
-            code: 404,
-            status: 'error',
-            data: { error: 'Transaction type not found' },
-          });
-        }
-      }
-
-      // Update transaksi
-      const updatedTransaction = await Transaction.findByIdAndUpdate(transactionId, body, {
-        new: true, // Mengembalikan data yang telah diperbarui
-        runValidators: true, // Pastikan validasi tetap dijalankan
-      });
-
-      res.status(200).json({
-        code: 200,
-        status: 'success',
-        data: updatedTransaction,
       });
     } catch (error) {
       return next(new ErrorHandler(error.message, 500));
@@ -170,6 +116,73 @@ router.get(
     }
   })
 );
+
+
+// Endpoint untuk memperbarui transaksi
+router.put(
+  '/:id',
+  isAuthenticated,
+  catchAsyncErrors(async (req, res, next) => {
+    const { body } = req;
+    const transactionId = req.params.id;
+
+    // Validasi input
+    const validationResponse = v.validate(body, {
+      ...transactionSchema,
+      mid: { ...transactionSchema.mid, optional: true },
+      transaction_type: { ...transactionSchema.transaction_type, optional: true },
+      status: { ...transactionSchema.status, optional: true },
+    });
+
+    if (validationResponse !== true) {
+      return res.status(400).json({
+        code: 400,
+        status: 'error',
+        data: {
+          error: 'Validation failed',
+          details: validationResponse,
+        },
+      });
+    }
+
+    try {
+      const transaction = await Transaction.findById(transactionId);
+      if (!transaction) {
+        return res.status(404).json({
+          code: 404,
+          status: 'error',
+          data: { error: 'Transaction not found' },
+        });
+      }
+
+      if (body.transaction_type) {
+        const transactionType = await TransactionType.findById(body.transaction_type);
+        if (!transactionType) {
+          return res.status(404).json({
+            code: 404,
+            status: 'error',
+            data: { error: 'Transaction type not found' },
+          });
+        }
+      }
+
+      // Update transaksi
+      const updatedTransaction = await Transaction.findByIdAndUpdate(transactionId, body, {
+        new: true,
+        runValidators: true,
+      });
+
+      res.status(200).json({
+        code: 200,
+        status: 'success',
+        data: updatedTransaction,
+      });
+    } catch (error) {
+      return next(new ErrorHandler(error.message, 500));
+    }
+  })
+);
+
 
 // Get transaction by ID
 router.get(
